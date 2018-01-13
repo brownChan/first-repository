e9cd232d5bf88e415c09ef81189df018115e30ad
jack <jack@qq.com>
2018-01-13 11:09:01 +0800

    test normalize

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,17 +1,26 @@
 <template>
-<div>
-{{msg}}test a test,look look root component yes or on input to the page
-</div>
+    <div>
+        公共头部
+        <router-view></router-view>
+        <el-rate
+            v-model="value2"
+            :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+        </el-rate>
+        公共底部
+    </div>
 </template>
+
 <script>
-export default{
-  data(){
-    return {
-      msg:"你妹的dfsdfsdfsfdsfsd"
+    export default {
+        data () {
+            return {
+                value2:2
+            }
+        }
     }
-  }
-}
 </script>
-<style>
+
+<style scoped>
 
 </style>
+
--- src/main.js
+++ src/main.js
@@ -2,17 +2,46 @@
  * @Author: 我家的小强 
  * @Date: 2018-01-12 11:06:20 
  * @Last Modified by: 我家的小强
- * @Last Modified time: 2018-01-12 16:05:03
+ * @Last Modified time: 2018-01-13 10:41:30
  */
-//1.导入第三方包
+//1.导入vue相关的第三方包
 import Vue from "vue";
+//导入css
+import "normalize.css";
+//导入路由插件
+import VueRouter from "vue-router";
+//导入element-ui
+import ElementUI from "element-ui";
+//导入element-ui样式
+import "../node_modules/element-ui/lib/theme-chalk/index.css";
+import "../node_modules/element-ui/lib/theme-chalk/icon.css";
+
+
+//使用element-ui
+Vue.use(ElementUI);
+
+//启动vue插件
+Vue.use(VueRouter);
+
 //导入自己写的东西
 import App from "./component/App.vue";
+//导入路由配置
+import routerConfig from "./router/index.js";
+
+//导入axios，因为他不是vue插件，使用起来不方便，所以我们手动把它加入到原型中，方便使用
+import axios from "axios";
+axios.defaults.baseURL = "http://157.122.54.189:9095"; //一配置，以后所有的请求就会自动使用这个域名
+Vue.prototype.$http = axios;
+
+//导入api配置对象，为了使用更加方便，把它也加入到原型中
+import api from "./js/api-config.js";
+Vue.prototype.$api = api;
 
 //把根组件渲染到指定视图
 new Vue({
   el:"#app",
   render: function (createElement) {
     return createElement(App);
-  }
+  },
+  router: new VueRouter(routerConfig)
 })
