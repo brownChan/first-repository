9c6295ccd5f219e11b0dca9b6be1dcf6d23ed08c
brownChan <cyt7758758@163.com>
2018-01-14 22:30:52 +0800

    公共左侧与头部

--- src/component/admin/common/Aside.vue
+++ src/component/admin/common/Aside.vue
@@ -1,15 +1,80 @@
 <template>
-  <div>
-    左侧
-  </div>
+  <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+
+      <!-- 一个菜单栏，记得配置index属性，不配置就会有问题 -->
+      <el-submenu v-for="item in menu" :key="item.title" :index="item.title">
+
+        <!-- 标题内容 -->
+        <template slot="title">
+          <i :class="item.icon"></i>
+          <span>{{item.title}}</span>
+        </template>
+        
+        <!-- 子菜单列表 -->
+          <el-menu-item v-for="subitem in item.children" :key="subitem.title" :index="subitem.title">
+            <router-link :to="subitem.path">{{subitem.title}}</router-link>
+          </el-menu-item>
+      </el-submenu>
+    </el-menu>
 </template>
   
 <script>
   export default {
-  
+    data(){
+      return {
+        //菜单很多项,所以是个大数组
+        //数组里面存放的数据很多，所以是一个对象
+        // 对象里面有个title字段
+        // 对象里面有个children字段存放的子菜单，是个数组
+        // children字段里面又放置了很多对象
+        // 有个title字段代表子菜单标题
+        // 有个path字段代表点击该标题时跳转的路径
+        menu:[
+          {title:"资源下载",icon:"el-icon-download",
+            children:[
+              {title:"内容管理",path:"/a"},
+              {title:"类别管理",path:"/b"},
+              {title:"评论管理",path:"/c"},
+            ]
+          },
+          {title:"购物商城",icon:"el-icon-goods",
+            children:[
+              {title:"内容管理",path:"/a"},
+              {title:"类别管理",path:"/b"},
+              {title:"评论管理",path:"/c"},
+            ]
+          },
+          {title:"订单管理",icon:"el-icon-tickets",
+            children:[
+              {title:"内容管理",path:"/a"},
+              {title:"类别管理",path:"/b"},
+              {title:"评论管理",path:"/c"},
+            ]
+          }
+        ]
+      }
+    },
+     methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
   }
 </script>
   
 <style scoped>
-  
+  a{
+    color: #fff;
+    text-decoration: none;
+  }
 </style>
--- src/component/admin/common/Header.vue
+++ src/component/admin/common/Header.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-    头部
-  </div>
+  <el-dropdown trigger="click" class="header">
+      <span class="el-dropdown-link">
+        <span>你好，{{uname}}</span>
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>修改信息</el-dropdown-item>
+        <!-- 注意：默认情况下，监听组件的事件都是监听自己emit发射的事件  -->
+        <!-- 如果要监听浏览器原生事件，就需要添加个native修饰符即可 -->
+        <el-dropdown-item @click.native ="logout">退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
 </template>
   
 <script>
   export default {
-  
+    data () {
+      return {
+        uname:"admin"
+      }
+    },
+    methods: {
+      logout () {
+        this.$confirm('正在退出, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http.get(this.$api.logout)
+          .then(res=>{
+              this.$router.push({name:"login"});
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'success',
+            message: '已取消退出'
+          });          
+        });
+      }
+    }
   }
 </script>
   
 <style scoped>
-  
+  .el-dropdown-link{
+    cursor: pointer;
+    color: rgba(255, 255, 255, 0.753);
+  }
+  .header{
+    display: block;
+    text-align: right;
+    line-height: 60px;
+  }
 </style>
