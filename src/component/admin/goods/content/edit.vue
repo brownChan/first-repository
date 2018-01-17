47b04efedd3bb45a779f7767acfbb86202470b8a
brownChan <cyt7758758@163.com>
2018-01-17 10:16:33 +0800

    完成图片上传和上传附件

--- src/component/admin/goods/content/edit.vue
+++ src/component/admin/goods/content/edit.vue
@@ -1,31 +1,37 @@
 <template>
   <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" style="width:500px">
     <!-- 标题 -->
-  <el-form-item label="标题" prop="name">
+  <el-form-item label="标题" prop="title">
     <el-input v-model="ruleForm.title"></el-input>
   </el-form-item>
 
   <!-- 副标题 -->
-  <el-form-item label="副标题" prop="fname">
+  <el-form-item label="副标题" prop="sub_title">
     <el-input v-model="ruleForm.sub_title"></el-input>
   </el-form-item>
 
   <!-- 所属类别 -->
-  <el-form-item label="所属类别" prop="region">
+  <el-form-item label="所属类别" prop="category_id">
     <!-- v-model绑定数据，将来要和每个option的value值进行匹配，匹配到哪个默认就会选中哪个 -->
     <el-select v-model="ruleForm.category_id" placeholder="请选择分类">
       <!-- goodsCategory是请求接口获取到的分类列表，将来用户要选择其中一个分类的ID传给后端 -->
-      <el-option v-for="item in goodsCategory" :key="item.category_id" :label="item.title" :value="item.category_id"></el-option>
+      <el-option v-for="item in goodsCategory" :key="item.category_id" :label="item.title" :value="item.category_id">
+        <!-- option标签内可以嵌套子标签，但是option的label属性不能删掉 -->
+        <span>
+          <span v-if="item.class_layer != 1">|- </span>
+          <span>{{ item.title }}</span>
+        </span>
+      </el-option>
     </el-select>
   </el-form-item>
 
   <!-- 是否发布 -->
-  <el-form-item label="是否发布" prop="delivery">
+  <el-form-item label="是否发布" prop="status">
     <el-switch v-model="ruleForm.status"></el-switch>
   </el-form-item>
 
   <!-- 推荐类型 -->
-  <el-form-item label="推荐类型" prop="delivery">
+  <el-form-item label="推荐类型">
     <el-switch v-model="ruleForm.is_slide" active-text="轮播图">
     </el-switch>
     <el-switch v-model="ruleForm.is_top" active-text="置顶">
@@ -36,38 +42,35 @@
     
   <!-- 上传封面 -->
   <el-form-item label="上传封面" prop="delivery">
-    <el-switch v-model="ruleForm.imgList">
-    </el-switch>
-    <!-- <el-upload class="upload-demo"
-    action="https://jsonplaceholder.typicode.com/posts/"
-    :on-preview = "handlePreview"
-    :on-remove ="handleRemove"
-    :file-list="fileList2"
-    list-type="picture">
-    <el-button size="small" type="primary">点击上传</el-button>
-    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-  </el-upload> -->
+    <!-- action属性用来设置图片上传地址，file-list属性用来关联图片列表 -->
+    <!-- on-preview与on-remove用来添加回调函数的，注意他们是属性的方式添加 -->
+    <el-upload class="upload-demo"
+      action="http://157.122.54.189:9095/admin/article/uploadimg"
+      :on-success = "imgUploaded"
+      :file-list="ruleForm.imgList" list-type="picture">
+      <el-button size="small" type ="primary">点击上传</el-button>
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+    </el-upload>
   </el-form-item>
   
 
   <!-- 上传文件 -->
   <el-form-item label="上传附件" prop="delivery">
-    <el-switch v-model="ruleForm.fileList">
-    </el-switch>
-    <!-- <el-upload
-      class="upload-demo"
-      action="https://jsonplaceholder.typicode.com/posts/"
-      :on-change="handleChange"
-      :file-list="fileList3">
+    <!-- action属性用来设置图片上传地址，file-list属性用来关联图片列表 -->
+    <!-- on-preview与on-remove用来添加回调函数的，注意他们是属性的方式添加 -->
+
+    <el-upload class="upload-demo"
+      action="http://157.122.54.189:9095/admin/article/uploadfile"
+      :on-success="fileUploaded" :file-list="ruleForm.fileList">
       <el-button size="small" type="primary">点击上传</el-button>
       <div slot="tip" class="el-upload__tip">只能上传txt/doc文件，且不超过500kb</div>
-    </el-upload> -->
+    </el-upload>
   </el-form-item>
   
 
 
   <!-- 货号 -->
-  <el-form-item label="货号" prop="desc">
+  <el-form-item label="货号" prop="name">
     <el-input type="text" v-model="ruleForm.goods_no"></el-input>
   </el-form-item>
 
@@ -114,7 +117,7 @@
         //分类列表数据
         goodsCategory: [],
         //上传图片
-      fileList2: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+      fileList2: [],
 
         //表单数据
         ruleForm: {},
@@ -123,7 +126,7 @@
         rules: {
           name: [
             { required: true, message: '请输入标题', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+            { min: 5, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
           ],
           fname: [
             { required: true, message: '请输入副标题', trigger: 'blur' },
@@ -132,17 +135,8 @@
           region: [
             { required: true, message: '请选择活动区域', trigger: 'change' }
           ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-          ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
+          delivery: [
+            { required: true, message: '请选择图片上传', trigger: 'change' }
           ],
           desc: [
             { required: true, message: '请填写活动形式', trigger: 'blur' }
@@ -186,7 +180,19 @@
         .then(res =>{
           this.goodsCategory = res.data.message;
         })
-      }
+      },
+      //上传图片成功的回调
+      //注意：商品的封面后端接口只能设一个，不能多个，所以每次我们上传成功后把原有的封面覆盖掉
+      imgUploaded(res, file, fileList){
+        // console.log(res,file,fileList);
+        this.ruleForm.imgList = [res];
+      },
+      //上传附件成功的回调
+      //注意：附件后端接口可以设多个，所以每次成功后我们在原来的基础上push新附件
+      fileUploaded (res, file, fileList) {
+        this.ruleForm.fileList.push(res);
+      },
+
     },
     //组件初始化之后，先拿到url中的id,在请求接口获取商品数据进行回显
     created () {
